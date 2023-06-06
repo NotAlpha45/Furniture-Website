5bba726b1ca105ddb056dd9a0128c4f16c893fd8
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2023-06-06 19:20:31 +0600

    Product reducer and store completed

--- src/action-constants/category-action-constant.js
+++ src/action-constants/category-action-constant.js
@@ -2,3 +2,5 @@ export const UPDATE_SELECTED_MAIN_CATEGORY = "update-selected-main-category";
 
 export const UPDATE_SELECTED_SUB_CATEGORY = "update-selected-sub-category";
 export const GET_SELECTED_SUB_CATEGORY = "get-selected-sub-category";
+
+
--- src/reducers/root-reducer.js
+++ src/reducers/root-reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import mainCategoryReducer from "./main-category-reducer";
 import subCategoryReducer from "./sub-category-reducer";
+import productReducer from "./furniture-reducer";
 import { createStore } from "redux";
 
 const combinedReducer = combineReducers({
   mainCategoryReducer,
   subCategoryReducer,
+  productReducer,
 });
 
 const combinedStore = createStore(combinedReducer);
